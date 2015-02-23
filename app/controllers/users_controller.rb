267c9df82f5878e94a44fea466e86d424512a3f9
Scott Fleming <Scott.Fleming@memphis.edu>
2015-02-23 03:40:30 +0000

    Added login and logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 
-  # def show
-  #   @user = User.find(params[:id])
-  # end
+  before_action :logged_in_user, only: [:show]
+  before_action :correct_user,   only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -11,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # !!!!!!!!! FIX THIS!!!!!!!!!!!
+      redirect_to @user
     else
       render 'new'
     end
@@ -25,6 +29,22 @@ class UsersController < ApplicationController
                                  :email,
                                  :password,
                                  :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
